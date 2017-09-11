d511c0cccec034898ffba06f9bcf40c8d9e3d331
carlitos4560 <carlitosrobertoes@gmail.com>
2017-09-11 00:49:15 -0400

    primeros pasos

--- crud-laravel/database/factories/UserFactory.php
+++ crud-laravel/database/factories/UserFactory.php
@@ -23,3 +23,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name'  => $faker->sentence(2),
+        'short' => $faker->text(140),
+        'body'  => $faker->text(900),
+    ];
+});
